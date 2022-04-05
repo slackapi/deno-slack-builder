1538e91de2df6d1a93bcf9130d223e722dafe353
Filip Maj <fmaj@slack-corp.com>
2022-04-05 11:19:48 -0400

    Factor out builder and runtime versions into constants.

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,21 +1,24 @@
+export const BUILDER_TAG = "deno_slack_builder@0.0.5";
+export const RUNTIME_TAG = "deno_slack_runtime@0.0.2";
+
 export const projectScripts = () => {
   return {
     "manifest": {
       "script": {
         "default":
-          "deno run -q --unstable --import-map=import_map.json --allow-read --allow-net https://deno.land/x/deno_slack_builder@0.0.5/mod.ts --manifest",
+          `deno run -q --unstable --import-map=import_map.json --allow-read --allow-net https://deno.land/x/${BUILDER_TAG}/mod.ts --manifest`,
       },
     },
     "package": {
       "script": {
         "default":
-          "deno run -q --unstable --import-map=import_map.json --allow-read --allow-write --allow-net https://deno.land/x/deno_slack_builder@0.0.5/mod.ts",
+          `deno run -q --unstable --import-map=import_map.json --allow-read --allow-write --allow-net https://deno.land/x/${BUILDER_TAG}/mod.ts`,
       },
     },
     "run": {
       "script": {
         "default":
-          "deno run -q --unstable --allow-write --allow-read --allow-net https://deno.land/x/deno_slack_runtime@0.0.2/mod.ts",
+          `deno run -q --unstable --allow-write --allow-read --allow-net https://deno.land/x/${RUNTIME_TIME}/mod.ts`,
       },
     },
   };
