fd641aa3cf892d8c5a2cdd02ce3a370d4f9da9a9
Neil Shapiro <nshapiro@slack-corp.com>
2022-04-28 17:32:26 -0500

    Make sure builder always validates function files (#18)

--- src/functions.ts
+++ src/functions.ts
@@ -1,17 +1,16 @@
 import { Options } from "./types.ts";
 import { ensureDir, path } from "./deps.ts";
 
-// deno-lint-ignore no-explicit-any
-export const createFunctions = async (options: Options, manifest: any) => {
-  if (!options.outputDirectory) {
-    throw new Error(
-      "Cannot build function files if no output option is provided",
-    );
+export const validateAndCreateFunctions = async (
+  options: Options,
+  // deno-lint-ignore no-explicit-any
+  manifest: any,
+) => {
+  if (options.outputDirectory) {
+    // Ensure functions directory exists
+    const functionsPath = path.join(options.outputDirectory, "functions");
+    await ensureDir(functionsPath);
   }
-
-  // Ensure functions directory exists
-  const functionsPath = path.join(options.outputDirectory, "functions");
-  await ensureDir(functionsPath);
 
   // Find all the run on slack functions
   for (const fnId in manifest.functions) {
@@ -23,68 +22,95 @@ export const createFunctions = async (options: Options, manifest: any) => {
     //   continue;
     // }
 
-    if (!fnDef.source_file) {
+    // Always validate function paths
+    const fnFilePath = await getValidFunctionPath(options, fnId, fnDef);
+
+    // Create function files if there is an output directory provided
+    if (options.outputDirectory) {
+      createFunctionFile(options as Required<Options>, fnId, fnFilePath);
+    } else if (!options.outputDirectory && !options.manifestOnly) {
+      // If no output directory and not just outputting manifest, throw error
       throw new Error(
-        `Run on Slack function provided for ${fnId}, but no source_file was provided.`,
+        "Cannot build function files if no output option is provided",
       );
     }
+  }
+};
 
-    const fnFilePath = path.join(options.workingDirectory, fnDef.source_file);
-
-    // Make sure it's a file
-    try {
-      const { isFile } = await Deno.stat(fnFilePath);
-      if (!isFile) {
-        throw new Error(`Could not find file: ${fnFilePath}`);
-      }
-    } catch (e) {
-      if (e instanceof Deno.errors.NotFound) {
-        throw new Error(
-          `Could not find file: ${fnFilePath}. Make sure your function's "source_file" is relative to your project root.`,
-        );
-      }
-      throw new Error(e);
-    }
-
-    // Bundle File
-    let isImportMapPresent = false;
-    const importMapPath = `${options.workingDirectory}/import_map.json`;
-
-    try {
-      const { isFile } = await Deno.stat(importMapPath);
-      isImportMapPresent = isFile;
-    } catch (_e) {
-      isImportMapPresent = false;
-    }
-
-    const result = await Deno.emit(fnFilePath, {
-      bundle: "module",
-      check: false,
-      importMapPath: isImportMapPresent ? importMapPath : undefined,
-    });
-
-    // Write FN File and sourcemap file
-    const fnFileRelative = path.join("functions", `${fnId}.js`);
-    const fnBundledPath = path.join(options.outputDirectory, fnFileRelative);
-    const fnSourcemapPath = path.join(
-      options.outputDirectory,
-      "functions",
-      `${fnId}.js.map`,
+const getValidFunctionPath = async (
+  options: Options,
+  fnId: string,
+  // deno-lint-ignore no-explicit-any
+  fnDef: any,
+) => {
+  if (!fnDef.source_file) {
+    throw new Error(
+      `Run on Slack function provided for ${fnId}, but no source_file was provided.`,
     );
+  }
 
-    options.log(`wrote function file: ${fnFileRelative}`);
-    try {
-      await Deno.writeTextFile(
-        fnBundledPath,
-        result.files["deno:///bundle.js"],
-      );
-      await Deno.writeTextFile(
-        fnSourcemapPath,
-        result.files["deno:///bundle.js.map"],
-      );
-    } catch (e) {
-      options.log(e);
-      throw new Error(`Error writing bundled function file: ${fnId}`, e);
+  const fnFilePath = path.join(options.workingDirectory, fnDef.source_file);
+
+  // Make sure it's a file that exists
+  try {
+    const { isFile } = await Deno.stat(fnFilePath);
+    if (!isFile) {
+      throw new Error(`Could not find file: ${fnFilePath}`);
     }
+  } catch (e) {
+    if (e instanceof Deno.errors.NotFound) {
+      throw new Error(
+        `Could not find file: ${fnFilePath}. Make sure your function's "source_file" is relative to your project root.`,
+      );
+    }
+    throw new Error(e);
+  }
+  return fnFilePath;
+};
+
+const createFunctionFile = async (
+  options: Required<Options>,
+  fnId: string,
+  fnFilePath: string,
+) => {
+  // Bundle File
+  let isImportMapPresent = false;
+  const importMapPath = `${options.workingDirectory}/import_map.json`;
+
+  try {
+    const { isFile } = await Deno.stat(importMapPath);
+    isImportMapPresent = isFile;
+  } catch (_e) {
+    isImportMapPresent = false;
+  }
+
+  const result = await Deno.emit(fnFilePath, {
+    bundle: "module",
+    check: false,
+    importMapPath: isImportMapPresent ? importMapPath : undefined,
+  });
+
+  // Write FN File and sourcemap file
+  const fnFileRelative = path.join("functions", `${fnId}.js`);
+  const fnBundledPath = path.join(options.outputDirectory, fnFileRelative);
+  const fnSourcemapPath = path.join(
+    options.outputDirectory,
+    "functions",
+    `${fnId}.js.map`,
+  );
+
+  options.log(`wrote function file: ${fnFileRelative}`);
+  try {
+    await Deno.writeTextFile(
+      fnBundledPath,
+      result.files["deno:///bundle.js"],
+    );
+    await Deno.writeTextFile(
+      fnSourcemapPath,
+      result.files["deno:///bundle.js.map"],
+    );
+  } catch (e) {
+    options.log(e);
+    throw new Error(`Error writing bundled function file: ${fnId}`, e);
   }
 };
--- src/mod.ts
+++ src/mod.ts
@@ -1,5 +1,5 @@
 import { cleanManifest, createManifest } from "./manifest.ts";
-import { createFunctions } from "./functions.ts";
+import { validateAndCreateFunctions } from "./functions.ts";
 import { Options } from "./types.ts";
 import { parse, path } from "./deps.ts";
 
@@ -46,9 +46,7 @@ const run = async () => {
   // Generate Manifest
   const generatedManifest = await createManifest(options);
 
-  if (!options.manifestOnly) {
-    await createFunctions(options, generatedManifest);
-  }
+  await validateAndCreateFunctions(options, generatedManifest);
 
   const prunedManifest = cleanManifest(generatedManifest);
 
