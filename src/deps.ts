2a46b5096eb8ab31fe48a147ee87248a755bbeeb
Filip Maj <fmaj@slack-corp.com>
2022-07-18 18:18:15 -0400

    Import from deep_merge directly instead of the mod.ts to avoid pulling in unnecessary code.

--- src/deps.ts
+++ src/deps.ts
@@ -1,4 +1,4 @@
-export { deepMerge } from "https://deno.land/std@0.134.0/collections/mod.ts";
+export { deepMerge } from "https://deno.land/std@0.134.0/collections/deep_merge.ts";
 export { parse } from "https://deno.land/std@0.134.0/flags/mod.ts";
 export { ensureDir } from "https://deno.land/std@0.134.0/fs/mod.ts";
 export * as path from "https://deno.land/std@0.134.0/path/mod.ts";
