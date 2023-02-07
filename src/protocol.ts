e21b7abf96392733778bb0a1316a16e56cccec6e
Filip Maj <fmaj@slack-corp.com>
2023-02-07 16:57:29 -0500

    add message boundary protocol

--- src/protocol.ts
+++ src/protocol.ts
@@ -1,7 +1,10 @@
 import { parse } from "./deps.ts";
 import { Protocol } from "./types.ts";
 
-export const SUPPORTED_NAMED_PROTOCOLS = ["dont-cross-the-streams"];
+export const SUPPORTED_NAMED_PROTOCOLS = [
+  "dont-cross-the-streams",
+  "message-boundaries",
+];
 
 export const BaseProtocol = function (
   loggerMethod: typeof console.log,
@@ -21,8 +24,27 @@ export const DontCrossTheStreamsProtocol: Protocol = {
   respond: console.log,
 };
 
+export const MessageBoundaryProtocol = function (args: string[]): Protocol {
+  const { boundary } = parse(
+    args,
+  );
+  if (!boundary) throw new Error("no boundary argument provided!");
+  return {
+    log: console.log,
+    error: console.log,
+    warn: console.log,
+    // deno-lint-ignore no-explicit-any
+    respond: (data: any) => {
+      console.log(boundary);
+      console.log(data);
+      console.log(boundary);
+    },
+  };
+};
+
 const PROTOCOL_MAP = {
   [SUPPORTED_NAMED_PROTOCOLS[0]]: DontCrossTheStreamsProtocol,
+  [SUPPORTED_NAMED_PROTOCOLS[1]]: MessageBoundaryProtocol,
 };
 
 export const getProtocolInterface = function (args: string[]): Protocol {
@@ -31,12 +53,17 @@ export const getProtocolInterface = function (args: string[]): Protocol {
   );
   if (protocolFromCLI) {
     if (SUPPORTED_NAMED_PROTOCOLS.includes(protocolFromCLI)) {
-      return PROTOCOL_MAP[protocolFromCLI];
+      const iface = PROTOCOL_MAP[protocolFromCLI];
+      if (typeof iface === "function") {
+        return iface(args);
+      } else {
+        return iface;
+      }
     }
   }
   // If protocol negotiation fails for any reason, return the base protocol
   // In the base protocol, if only a manifest is being requested, then we must
   // return the manifest JSON over stdout, so the logging interface passed into
-  // BaseProtocol is a no-op function (to prevent logging to stdout)
+  // BaseProtocol is a no-op function (to prevent logging to stdout
   return BaseProtocol(manifestOnly ? () => {} : console.log);
 };
